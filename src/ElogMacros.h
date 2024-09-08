05d07bab15e16caf949e2e81c490066febe7cd6a
Martin Polehla <p0l0us@users.noreply.github.com>
2024-09-08 12:26:37 +0200

    Update ElogMacros.h

--- src/ElogMacros.h
+++ src/ElogMacros.h
@@ -10,55 +10,55 @@
  * @param message the message
  * @param ... the arguments
  */
-#define debug(logId, message, ...) logger.log(logId, DEBUG, message, ##__VA_ARGS__)
+#define LOG_D(logId, message, ...) logger.log(logId, DEBUG, message, ##__VA_ARGS__)
 
 /** info
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define info(logId, message, ...) logger.log(logId, INFO, message, ##__VA_ARGS__)
+#define LOG_I(logId, message, ...) logger.log(logId, INFO, message, ##__VA_ARGS__)
 
 /** notice
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define notice(logId, message, ...) logger.log(logId, NOTICE, message, ##__VA_ARGS__)
+#define LOG_N(logId, message, ...) logger.log(logId, NOTICE, message, ##__VA_ARGS__)
 
 /** warning
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define warning(logId, message, ...) logger.log(logId, WARNING, message, ##__VA_ARGS__)
+#define LOG_W(logId, message, ...) logger.log(logId, WARNING, message, ##__VA_ARGS__)
 
 /** error
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define error(logId, message, ...) logger.log(logId, ERROR, message, ##__VA_ARGS__)
+#define LOG_E(logId, message, ...) logger.log(logId, ERROR, message, ##__VA_ARGS__)
 
 /** critical
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define critical(logId, message, ...) logger.log(logId, CRITICAL, message, ##__VA_ARGS__)
+#define LOG_C(logId, message, ...) logger.log(logId, CRITICAL, message, ##__VA_ARGS__)
 
 /** alert
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define alert(logId, message, ...) logger.log(logId, ALERT, message, ##__VA_ARGS__)
+#define LOG_A(logId, message, ...) logger.log(logId, ALERT, message, ##__VA_ARGS__)
 
 /** emergency
  * @param logId the log id
  * @param message the message
  * @param ... the arguments
  */
-#define emergency(logId, message, ...) logger.log(logId, EMERGENCY, message, ##__VA_ARGS__)
+#define LOG_EM(logId, message, ...) logger.log(logId, EMERGENCY, message, ##__VA_ARGS__)
 
 #endif // ELOGMACROS_H
